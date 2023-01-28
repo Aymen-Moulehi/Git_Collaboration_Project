209fc76d38774398f966a784cbd47820be07c668
DELL <gherianimohamed10@gmail.com>
2023-01-28 15:06:30 +0100

    Adding Class_Gheriani_Mohamed

---
+++ src/classes/Class_Gheriani_Mohamed.java
@@ -0,0 +1,12 @@
+package classes;
+
+import java.util.function.Function;
+
+public class Class_Gheriani_Mohamed {
+
+    public void hello()
+        {
+        System.out.println("Hello I'am Gheriani Mohamed!");
+        }
+
+}
