34c1e45f406d6d017d62ee5595eabfd0ba0abcf0
baligh8 <obaligh8@gmail.com>
2023-01-28 15:33:55 +0100

    add class ClassBalighOthman

--- src/classes/ClassBalighOthman.java
+++ src/classes/ClassBalighOthman.java
@@ -4,7 +4,6 @@ public class ClassBalighOthman {
     public int ba ;
     public int Baligh() {
         System.out.println("hello i'am baligh");
-        System.out.println("hello i'am baligh");
         return ba ;
     }
 
